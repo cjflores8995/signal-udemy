1be297edb1eef0505bf3d5cfbba17b0854c78688
= <carlos_jhousef593@hotmail.com>
2020-10-26 21:25:30 -0500

    contador de visitas

---
+++ Hub-01-ContadorVisitas/Hubs/ContadorHub.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Hub_01_ContadorVisitas.Hubs
+{
+    public class ContadorHub : Hub
+    {
+        private static int _visitantes = 0;
+
+        public override async Task OnConnected()
+        {
+            Interlocked.Increment(ref _visitantes);
+
+            await Clients.Others.Message("Nueva conexión: " + Context.ConnectionId + " | Total visitantes: " + _visitantes);
+            await Clients.Caller.Message("Hola bienvenido!");
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            Interlocked.Decrement(ref _visitantes);
+            return Clients.All.Message(Context.ConnectionId + " se ha desconectado | Total visitantes: " + _visitantes);
+        }
+
+        public Task Broadcast(string message)
+        {
+            return Clients.All.Message(Context.ConnectionId + ">> " + message);
+        }
+    }
+}
