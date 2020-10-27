cc909c26eb83821fcd4086438e0f561dbee8b8ae
= <carlos_jhousef593@hotmail.com>
2020-10-26 20:36:19 -0500

    broadcast con hubs para mensajes a todos los usuarios

---
+++ Hub-01-Broadcast/Controllers/BroadcastController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hub_01_Broadcast.Controllers
+{
+    public class BroadcastController : Controller
+    {
+        // GET: Broadcast
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Hub-01-Broadcast/Hubs/BroadcastHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Hub_01_Broadcast.Hubs
+{
+    public class BroadcastHub : Hub
+    {
+        public void BroadcastMessage(string message)
+        {
+            Clients.All.displayText(message);
+        }
+    }
+}
